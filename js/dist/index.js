0a0dc50b847fabcdc9f69b5daaa8493cf669baa2
aniket panchal <aniketpanchal07@gmail.com>
2024-06-16 15:40:27 +0530

    changes in index.ts

--- js/dist/index.js
+++ js/dist/index.js
@@ -2,7 +2,8 @@
 document.addEventListener("DOMContentLoaded", function () {
     const canvas = document.querySelector("canvas");
     const c = canvas === null || canvas === void 0 ? void 0 : canvas.getContext("2d");
-    canvas === null || canvas === void 0 ? void 0 : canvas.width = 1024;
-    canvas === null || canvas === void 0 ? void 0 : canvas.height = 576;
-    // Now you can safely use the canvas and context
+    canvas.width = 1024;
+    canvas.height = 576;
+
+
 });
--- js/src/index.ts
+++ js/src/index.ts
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const canvas = document.querySelector("canvas");
     const c = canvas?.getContext("2d");
 
-    canvas?.width=1024
-    canvas?.height=576
+    canvas!.width=1024
+    canvas!.height=576
+    
     // Now you can safely use the canvas and context
 });
